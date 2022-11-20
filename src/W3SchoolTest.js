2babc98c1c04fd77ae1b80053083a8c5eff84405
Bappi Kabir <kabirbapson@gmail.com>
2022-11-20 11:47:55 +0100

    personalized icon pending

--- src/W3SchoolTest.js
+++ src/W3SchoolTest.js
@@ -1,31 +1,27 @@
 import React from "react";
 
 export default function W3SchoolTest() {
+  const onCLick = () => {
+    document.getElementById("clicks").innerHTML = "Pinged";
+  };
+  const onCLick1 = () => {
+    document.getElementById('demo').innerHTML = Date()
+  };
   window.addEventListener("DOMContentLoaded", () => {
     document.getElementById("id1").innerHTML = "hari";
-    function toCelsius(fahrenheit) {
-      return (5 / 9) * (fahrenheit - 32);
-    }
-    document.getElementById("demo").innerHTML = toCelsius(77);
+
+    // document.getElementById("demo").innerHTML =
   });
-  var a = 10 % 4;
-  // console.log(a)
-  let x = 100 + 50 * 3;
-  x = 100 + 50 - 3;
-  // console.log(x)
-  var answer3 = 'He is called "Johnny"';
-  // console.log(answer3)
-  var car = "";
-  // console.log(typeof car)
-  function abc(a, b) {
-    x = a * b;
-    return x;
-  }
-  console.log(abc(4, 2));
+console.log('hello \nboss')
   return (
     <div>
       <h1 id="id1">f</h1>
       <h1 id="demo">f</h1>
+      <h2 id="clicks">f</h2>
+      <button onClick={onCLick}>Hit Me</button>
+
+      <button onClick={onCLick1}>Hit Me</button>
+      <button onClick={()=>document.getElementById('demo').innerHTML = Date().slice(2,10)}>The time is?</button>
     </div>
   );
 }
