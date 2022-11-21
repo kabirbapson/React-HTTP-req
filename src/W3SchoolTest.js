56f0fcac472298f1e7c56a5ae5133dbf165fbb5d
Bappi Kabir <kabirbapson@gmail.com>
2022-11-21 05:26:14 +0100

    personalizedpending

--- src/W3SchoolTest.js
+++ src/W3SchoolTest.js
@@ -1,30 +1,39 @@
 import React from "react";
 
 export default function W3SchoolTest() {
-  const onCLick = () => {
-    document.getElementById("clicks").innerHTML = "Pinged";
-  };
-  const onCLick1 = () => {
-    document.getElementById("demo").innerHTML = Date();
-  };
   window.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("id1").innerHTML = "hari";
-
+    // document.getElementById("id1").innerHTML = "hari";
     // document.getElementById("demo").innerHTML =
+    //  document.getElementById('id1').innerHTML = 85
   });
-  console.log("hello \nboss");
-  return (
-    <div>
-      <h1 id="id1">f</h1>
-      <h1 id="demo">ff</h1>
-      <h2 id="clicks">f</h2>
-      <button onClick={onCLick}>Hit Me</button>
+  var cars = ["Saab", "Volvo", "BMW"];
+  for (let i = 0; i < cars.length; i = i + 1) {
+    console.log(i, cars[i]);
+  }
+  var fruits, text, fLen, i;
+  fruits = ["Banana", "Orange", "Apple", "Mango"];
+  fLen = fruits.length;
 
-      <button onClick={onCLick1}>Hit Me</button>
+  text = "<ul>";
+  for (i = 0; i < fLen; i++) {
+    text += "<li>" + fruits[i] + "</li>";
+  }
+  text += "</ul>";
+console.log(text);
+  return (
+    <div style={{ justifyContent: "center", alignItems: "center" }}>
+      <h1 id="id1">f</h1>
+      {/* {document.write('ddd')} */}
+      {/* <h1 id="demo">ff</h1> */}
+      {/* <h2 id="clicks">f</h2> */}
+      {/* <button onClick={onCLick}>Hit Me</button> */}
+
+      {/* <button onClick={onCLick1}>Hit Me</button> */}
       <button
-        onClick={() =>
-          (document.getElementById("demo").innerHTML = Date().slice(2, 10))
-        }
+        onClick={() => window.alert("dddj jjd")}
+        // onClick={() =>
+        //   (document.getElementById("demo").innerHTML = Date().slice(2, 10))
+        // }
       >
         The time is?
       </button>
