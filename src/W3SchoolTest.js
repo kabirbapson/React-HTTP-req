e8b9b832b565fd1b955f130d820ad554bbdc0431
Bappi Kabir <kabirbapson@gmail.com>
2022-11-24 15:09:36 +0100

    native devices

--- src/W3SchoolTest.js
+++ src/W3SchoolTest.js
@@ -6,29 +6,21 @@ export default function W3SchoolTest() {
     // document.getElementById("demo").innerHTML =
     //  document.getElementById('id1').innerHTML = 85
   });
-  var m = "ahijo, hdd, dddd";
-  var fruits = ["Banana", "ido", "Apple", "Mango", "ido"];
 
-  var d = new Date(100000000000);
-  console.log("ffff", d);
+  var date = new Date().getHours()
+  console.log(date < 18);
+  if (date < 12) {
+    console.log('object morning')
+  }else {console.log('aft')}
 
-  var day = new Date("October 13, 2000");
-  console.log(day);
   return (
-    <div style={{ justifyContent : "center", alignItems: "center" }}>
+    <div style={{ justifyContent: "center", alignItems: "center" }}>
       <h1 id="id1">f</h1>
-      {/* {document.write('ddd')} */}
-      {/* <h1 id="demo">ff</h1> */}
-      {/* <h2 id="clicks">f</h2> */}
-      {/* <button onClick={onCLick}>Hit Me</button> */}
 
-      {/* <button onClick={onCLick1}>Hit Me</button> */}
       <button
-        onClick={() => window.alert("dddj jjd")}
-        // onClick={() =>
-
-        //   (document.getElementById("demo").innerHTML = Date().slice(2, 10))
-        // }
+        onClick={() =>
+          (document.getElementById("id1").innerHTML = new Date().getMonth() + 1)
+        }
       >
         The time is?
       </button>
