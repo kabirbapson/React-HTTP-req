c7bebb8882b15ee41c234c320bb0ec8226f4acda
Bappi Kabir <kabirbapson@gmail.com>
2022-11-25 01:34:28 +0100

    native devices

--- src/W3SchoolTest.js
+++ src/W3SchoolTest.js
@@ -6,17 +6,16 @@ export default function W3SchoolTest() {
     // document.getElementById("demo").innerHTML =
     //  document.getElementById('id1').innerHTML = 85
   });
-  var dd = new Date()
-  var d = '5'*2
-console.log(typeof d);
+  var dd = new Date();
+  var d = "5" * 2;
+  console.log(typeof d);
   return (
     <div style={{ justifyContent: "center", alignItems: "center" }}>
       <h1 id="id1">f</h1>
 
       <button
         onClick={() =>
-        (document.getElementById("id1").innerHTML
-          = new Date().getMonth() + 1)
+          (document.getElementById("id1").innerHTML = new Date().getMonth() + 1)
         }
       >
         The time is?
