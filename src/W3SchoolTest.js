b76e4f95b13af911caf29009346373d9dd79747b
Bappi Kabir <kabirbapson@gmail.com>
2022-11-25 01:34:10 +0100

    native devices

--- src/W3SchoolTest.js
+++ src/W3SchoolTest.js
@@ -6,20 +6,17 @@ export default function W3SchoolTest() {
     // document.getElementById("demo").innerHTML =
     //  document.getElementById('id1').innerHTML = 85
   });
-
-  var date = new Date().getHours()
-  console.log(date < 18);
-  if (date < 12) {
-    console.log('object morning')
-  }else {console.log('aft')}
-
+  var dd = new Date()
+  var d = '5'*2
+console.log(typeof d);
   return (
     <div style={{ justifyContent: "center", alignItems: "center" }}>
       <h1 id="id1">f</h1>
 
       <button
         onClick={() =>
-          (document.getElementById("id1").innerHTML = new Date().getMonth() + 1)
+        (document.getElementById("id1").innerHTML
+          = new Date().getMonth() + 1)
         }
       >
         The time is?
