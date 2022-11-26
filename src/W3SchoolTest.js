36f97cb56adbbdfb917d40ed7cfb3e6472542567
Bappi Kabir <kabirbapson@gmail.com>
2022-11-27 00:28:45 +0100

    native devices

--- src/W3SchoolTest.js
+++ src/W3SchoolTest.js
@@ -1,13 +1,17 @@
 import React from "react";
 
 export default function W3SchoolTest() {
-  window.addEventListener("DOMContentLoaded", () => {
-    // document.getElementById("id1").innerHTML = "hari";
-    // document.getElementById("demo").innerHTML =
-    //  document.getElementById('id1').innerHTML = 85
-  });
+ 
+  class Car {
+    constructor(made) {
+      this.carName = made
+    }
+  }
 
-console.log(eval(2+3));
+  let make = new Car('Ford')
+console.log(make['carName'])
+
+
   return (
     <div style={{ justifyContent: "center", alignItems: "center" }}>
       <h1 id="id1">f</h1>
@@ -16,7 +20,7 @@ console.log(eval(2+3));
           (document.getElementById("id1").innerHTML = new Date().getMonth() + 1)
         }
       >
-        The time is? 
+        The time is?
       </button>
     </div>
   );
