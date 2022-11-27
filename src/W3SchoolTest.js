5ba5c0390c50532837ed908fb744da5286f6b6b0
Bappi Kabir <kabirbapson@gmail.com>
2022-11-27 02:01:01 +0100

    native devices

--- src/W3SchoolTest.js
+++ src/W3SchoolTest.js
@@ -1,28 +1,8 @@
 import React from "react";
 
 export default function W3SchoolTest() {
-  class Car {
-    constructor(brand) {
-      this.carName = brand;
-    }
-    present(x) {
-      return "i have a  " + this.carName + x.make
-    }
-  }
-  class Made extends Car {
-    constructor(brand, mode) {
-      super(brand);
-      this.mode = mode;
-    }
-    show() {
-      return this.carName + this.mode
-    }
-    make() {
-      return 'hwy'
-    }
-  }
-  const myCar = new Made('Ford', 'Mustang')
-  console.log(myCar.make())
+  let x = 20;
+  console.log(typeof x)
   return (
     <div style={{ justifyContent: "center", alignItems: "center" }}>
       <h1 id="id1">f</h1>
