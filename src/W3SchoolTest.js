666f3032c0d70c7b900e4ab025429c4d5bc074ca
Bappi Kabir <kabirbapson@gmail.com>
2022-12-02 15:35:36 +0100

    update

--- src/W3SchoolTest.js
+++ src/W3SchoolTest.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function W3SchoolTest() {
   var x = 6;
   if (x == 7) {
-    console.log("fi rs t");
+    console.log("firs t");
   }
   return (
     <div style={{ justifyContent: "center", alignItems: "center" }}>
