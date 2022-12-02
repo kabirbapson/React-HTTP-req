14bdc8ee9258f20d9b0244df662da71de04deb79
Bappi Kabir <kabirbapson@gmail.com>
2022-12-02 02:04:26 +0100

    redesign profile screen

--- src/W3SchoolTest.js
+++ src/W3SchoolTest.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function W3SchoolTest() {
   var x = 6;
   if (x == 7) {
-    console.log("first");
+    console.log("fi rst");
   }
   return (
     <div style={{ justifyContent: "center", alignItems: "center" }}>
