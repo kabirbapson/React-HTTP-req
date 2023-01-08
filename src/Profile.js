3fccc7da695e349d1893912016b4a2e19423d327
Bappi Kabir <kabirbapson@gmail.com>
2023-01-08 07:15:36 +0100

    fix rtk

--- src/Profile.js
+++ src/Profile.js
@@ -8,10 +8,11 @@ const Profile = () => {
   const theme = useSelector((state) => state.theme.value);
   React.useEffect(() => {
     setColor(theme);
+    
   }, []);
   console.log("color", color);
   console.log("theme", theme);
-  
+
   
   
   const dispatch = useDispatch();
