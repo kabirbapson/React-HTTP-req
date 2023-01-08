8b823c545ea7a24f4b66b14ba579bded5396652f
Bappi Kabir <kabirbapson@gmail.com>
2023-01-08 07:15:28 +0100

    fix rtk

--- src/Profile.js
+++ src/Profile.js
@@ -13,10 +13,11 @@ const Profile = () => {
   console.log("theme", theme);
   
   
+  
   const dispatch = useDispatch();
 
 
-  
+
   return (
     <div style={{ color: theme }}>
       <h2>Name: {user.name}</h2>
