03bd9f5ef283a546fad43db8281c543ef7ab8053
Bappi Kabir <kabirbapson@gmail.com>
2023-01-18 02:27:01 +0100

    personal

--- src/Profile.js
+++ src/Profile.js
@@ -9,23 +9,13 @@ const Profile = () => {
   const theme = useSelector((state) => state.theme.value);
   React.useEffect(() => {
     setColor(theme);
-
-
-
-    
   }, []);
 
   console.log("color", color);
-  
-
-  
-
 
   console.log("theme", theme);
 
   const dispatch = useDispatch();
-
-
 
   return (
     <div style={{ color: theme }}>
