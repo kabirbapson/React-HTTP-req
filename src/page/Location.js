ef6e76bbee7b7fcda96e8726d6cc1ecc733fe16b
Bappi Kabir <kabirbapson@gmail.com>
2023-02-22 16:11:03 +0100

    added _redirects

--- src/page/Location.js
+++ src/page/Location.js
@@ -1,23 +1,28 @@
 import React from "react";
 
 export default function Location() {
-    const [location, setLocation] = React.useState();
-    const getLocation = () => {
-        navigator.geolocation.getCurrentPosition((position) => {
-            // setLocation(position);
-          console.log("position");
-          console.log(position);
-        },
-          (err) => {
-            console.log(err);
-          })
-    }
+  const [location, setLocation] = React.useState();
+  const getLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // setLocation(position);
+        console.log("position");
+        console.log(position);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  };
   React.useEffect(() => {
-   
     //   document.exitFullscreen()
-   
-     console.log( navigator.geolocation.getCurrentPosition(position=>console.log(position)))
-        //   .getCurrentPosition((position) => {
+
+    console.log(
+      navigator.geolocation.getCurrentPosition((position) =>
+        console.log(position)
+      )
+    );
+    //   .getCurrentPosition((position) => {
     //     setLocation(position);
     //   console.log("position");
     //   console.log(position);
@@ -25,16 +30,15 @@ export default function Location() {
     // { enableHighAccuracy: false, timeOut: 20000, maximumAge: 1000 }
   }, []);
 
-    return <div>
-        
-        <button onClick={getLocation}>
-        Request Permission
-      </button>
+  return (
+    <div>
+      <button onClick={getLocation}>Request Permission</button>
       {location && (
-      <div>
-        <p>Latitude: {location.latitude}</p>
-        <p>Longitude: {location.longitude}</p>
-      </div>
-    )}
-  </div>;
+        <div>
+          <p>Latitude: {location.latitude}</p>
+          <p>Longitude: {location.longitude}</p>
+        </div>
+      )}
+    </div>
+  );
 }
