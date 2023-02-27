047c39eb780070251cfe959fa85cddfcda439424
Bappi Kabir <kabirbapson@gmail.com>
2023-02-27 01:39:51 +0100

    new Designn

--- starter.js
+++ starter.js
@@ -1,18 +1,17 @@
-
-const meal = ["soup", "steak", "ice cream"]
-let [,eds] = meal;
+const meal = ["soup", "steak", "ice cream"];
+let [, eds] = meal;
 console.log(eds);
 
 let set = new Set();
-    set.add(1);
-    set.add(2);
-    set.add(3);
-    set.add(2);
-    set.add(1);
+set.add(1);
+set.add(2);
+set.add(3);
+set.add(2);
+set.add(1);
 console.log(set);
 
 function count(undefined) {
-    console.log(undefined.length)
+  console.log(undefined.length);
 }
 
 count(10, 9, 8, 7, 6);
