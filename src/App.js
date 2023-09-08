78090b572114d3e4d1709e6d453dd5193bfefad8
Bappi Kabir <kabirbapson@gmail.com>
2023-09-08 18:14:31 +0100

    exc3

--- src/App.js
+++ src/App.js
@@ -2,23 +2,20 @@ import React, { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-
-
   const audioRef = useRef(null);
   const fileInputRef = useRef(null);
 
   const handlePlay = () => {
     if (audioRef.current) {
-      audioRef.current.play()
-        .catch((error) => {
-          console.log(error);
-        });
+      audioRef.current.play().catch((error) => {
+        console.log(error);
+      });
     }
   };
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
-    
+
     if (file) {
       // Set the selected file as the source of the audio element
       audioRef.current.src = URL.createObjectURL(file);
@@ -26,26 +23,25 @@ function App() {
   };
   return (
     <div className="App">
-      <h1>
-        time is 
-      </h1>
+      <h1>time is</h1>
 
-      
       <div>
-      <h1>Audio Player</h1>
-      <audio ref={audioRef} controls>
-        Your browser does not support the audio element.
-      </audio>
-      <input
-        type="file"
-        accept="audio/*"
-        onChange={handleFileChange}
-        ref={fileInputRef}
-        style={{ display: 'none' }}
-      />
-      <button onClick={handlePlay}>Play</button>
-      <button onClick={() => fileInputRef.current.click()}>Select Audio File</button>
-    </div>
+        <h1>Audio Player</h1>
+        <audio ref={audioRef} controls>
+          Your browser does not support the audio element.
+        </audio>
+        <input
+          type="file"
+          accept="audio/*"
+          onChange={handleFileChange}
+          ref={fileInputRef}
+          style={{ display: "none" }}
+        />
+        <button onClick={handlePlay}>Play</button>
+        <button onClick={() => fileInputRef.current.click()}>
+          Select Audio File
+        </button>
+      </div>
       <button onClick={handlePlay}>Get Time</button>
     </div>
   );
