95c380bc02ef5d2d439b9a89bf2521a2f5bf204b
Bappi Kabir <kabirbapson@gmail.com>
2023-09-08 18:03:54 +0100

    exc3

--- src/App.js
+++ src/App.js
@@ -1,43 +1,53 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [names, setNames] = useState({ fname: "1", lname: "2" });
 
-  const updateFName = (e) => {
-    const { name, value } = e.target;
 
-    setNames((prevState) => {
-      return {
-        ...prevState,
-        [name]: value,
-      };
-    });
+  const audioRef = useRef(null);
+  const fileInputRef = useRef(null);
+
+  const handlePlay = () => {
+    if (audioRef.current) {
+      audioRef.current.play()
+        .catch((error) => {
+          console.log(error);
+        });
+    }
   };
 
-  const arr = [10, 20, 30, 40, 50];
-  const find = arr.findIndex((x) => x === 20);
-  console.log(find);
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    
+    if (file) {
+      // Set the selected file as the source of the audio element
+      audioRef.current.src = URL.createObjectURL(file);
+    }
+  };
   return (
     <div className="App">
       <h1>
-        time is {names.fname} {names.lname}{" "}
+        time is 
       </h1>
 
       
+      <div>
+      <h1>Audio Player</h1>
+      <audio ref={audioRef} controls>
+        Your browser does not support the audio element.
+      </audio>
+        <input type="file"  id="" />
       <input
-        value={names.fname}
-        name="fname"
-        placeholder="Fname"
-        onChange={updateFName}
+        type="file"
+        accept="audio/*"
+        onChange={handleFileChange}
+        ref={fileInputRef}
+        style={{ display: 'none' }}
       />
-      <input
-        value={names.lname}
-        name="lname"
-        placeholder="Lname"
-        onChange={updateFName}
-      />
-      <button onClick={() => console.log(names)}>Get Time</button>
+      <button onClick={handlePlay}>Play</button>
+      <button onClick={() => fileInputRef.current.click()}>Select Audio File</button>
+    </div>
+      <button onClick={handlePlay}>Get Time</button>
     </div>
   );
 }
