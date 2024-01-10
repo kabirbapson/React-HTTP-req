f0f49bd8f7b38e19cf96b6b23ef4fd6101b6dd4c
Bappi Kabir <kabirbapson@gmail.com>
2024-01-10 23:31:05 +0100

    build fixed

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -18,6 +18,7 @@ const ImageUpload = () => {
     formData.append("subject", "Image Attachment");
     formData.append("message", "This email contains an attached image.");
 
+    
     axios
       .post("127.0.0.1:3000/sendmail", formData, {
         headers: {
