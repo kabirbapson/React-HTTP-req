b4d5df1359bd9ff670b0bd61eb92ca0bbc5510b2
Bappi Kabir <kabirbapson@gmail.com>
2024-01-15 21:09:02 +0100

    paymentes

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -18,8 +18,6 @@ const ImageUpload = () => {
     formData.append("subject", "Image Attachment");
     formData.append("message", "This email contains an attached image.");
 
-    
-    
     axios
       .post("127.0.0.1:3000/sendmail", formData, {
         headers: {
@@ -35,6 +33,7 @@ const ImageUpload = () => {
     console.log("object");
   };
 
+  
   return (
     <div>
       <input type="file" onChange={handleImageSelect} />
